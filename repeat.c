221dbfbb80c7406e4a060ca6afb2228650bc9265
CollinBardini <cjbardini@gmail.com>
2015-04-08 14:13:28 -0700

    Update repeat.c

--- repeat.c
+++ repeat.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+int counter(const char *x)
+{
+	int y;
+	while(*x != '\0')
+	{
+		if(*x = ' ')
+			y++;
+		*x++;
+	}
+	return ++y;
+}
 int main(void)
 {
 	char x[80];
 	fgets(x,80,stdin);
 	puts(x);
 	puts(x);
+	int x = counter(x);
 	return 0;
 }
